402e1100b0a4eac764eba79e08886fcc0d410f35
Ian Bicking <ian@ianbicking.org>
2009-10-27 12:45:12 -0500

    update repo links

--- setup.py
+++ setup.py
@@ -9,8 +9,11 @@ setup(name='INITools',
 A set of tools for parsing and using ``.ini``-style files, including
 an abstract parser and several tools built on that parser.
 
-Repository available at `http://svn.colorstudy.com/INITools/trunk
-<http://svn.colorstudy.com/INITools/trunk#egg=INITools-dev>`_
+Repository available at `http://bitbucket.org/ianb/initools
+<http://bitbucket.org/ianb/initools>`_, or `download a tarball
+of the development version 
+<http://bitbucket.org/ianb/initools/get/tip.gz#egg=INITools-dev>`_
+using ``easy_install INITools==dev``
 """,
       classifiers=[
         "Intended Audience :: Developers",
